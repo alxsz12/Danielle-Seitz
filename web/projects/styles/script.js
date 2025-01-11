c743390e63f0ce0d98af78d11dd7aef42a10cb99
alxsz12 <alexseitz12@gmail.com>
2025-01-11 15:25:59 -0600

    Updates (A Lot)
    * Added a favicon for the top right of the files so that the tab has a logo
    * Added a new Mosaic for the main front page
    * Changed the about Danielle page to better show the profile
    * Changed some of the colors for the pages
    * Made the from exist already on below the links
    * Changed the images in the portfolio to have a card background
    * Added the arrows that allow the user to go to the next project with easier navigation
    * Added some design tweaks to the projects to fill some empty space
    * Reformatted some of the scripts for the arrows to make them more noticeable and easier to use
    * Redeployed to firebase
    * Cleaned up and reformatted the code

--- web/projects/styles/script.js
+++ web/projects/styles/script.js
@@ -1,21 +1,56 @@
 const mainImage = document.getElementById('mainImage');
-        const galleryImages = document.querySelectorAll('.image-gallery img');
-        const gallery = document.querySelector('.image-gallery');
-        const prevBtn = document.querySelector('.gallery-nav.prev');
-        const nextBtn = document.querySelector('.gallery-nav.next');
+const galleryImages = document.querySelectorAll('.image-gallery img');
+const gallery = document.querySelector('.image-gallery');
+const prevBtn = document.querySelector('.gallery-nav.prev');
+const nextBtn = document.querySelector('.gallery-nav.next');
 
-        galleryImages.forEach(img => {
-            img.addEventListener('click', () => {
-                mainImage.src = img.src;
-                galleryImages.forEach(i => i.classList.remove('active'));
-                img.classList.add('active');
-            });
-        });
+let currentImageIndex = 0;
+let slideInterval;
+const SLIDE_DELAY = 3000; // 3 seconds between slides
 
-        prevBtn.addEventListener('click', () => {
-            gallery.scrollBy({ left: -200, behavior: 'smooth' });
-        });
+function showImage(index) {
+    // Handle wrapping around
+    if (index >= galleryImages.length) index = 0;
+    if (index < 0) index = galleryImages.length - 1;
+    
+    currentImageIndex = index;
+    mainImage.src = galleryImages[index].src;
+    galleryImages.forEach(i => i.classList.remove('active'));
+    galleryImages[index].classList.add('active');
+}
 
-        nextBtn.addEventListener('click', () => {
-            gallery.scrollBy({ left: 200, behavior: 'smooth' });
-        });
+function startSlideshow() {
+    stopSlideshow(); // Clear any existing interval
+    slideInterval = setInterval(() => {
+        showImage(currentImageIndex + 1);
+    }, SLIDE_DELAY);
+}
+
+function stopSlideshow() {
+    if (slideInterval) {
+        clearInterval(slideInterval);
+    }
+}
+
+// Event Listeners
+galleryImages.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        showImage(index);
+        stopSlideshow(); // Stop auto-sliding when user interacts
+    });
+});
+
+prevBtn.addEventListener('click', () => {
+    gallery.scrollBy({ left: -200, behavior: 'smooth' });
+    showImage(currentImageIndex - 1);
+    stopSlideshow();
+});
+
+nextBtn.addEventListener('click', () => {
+    gallery.scrollBy({ left: 200, behavior: 'smooth' });
+    showImage(currentImageIndex + 1);
+    stopSlideshow();
+});
+
+// Start the slideshow when the page loads
+startSlideshow();
